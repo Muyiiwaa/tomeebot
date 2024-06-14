9b6cbb54db1fb73c9fca355caffe91eeb1074bb8
Muyiwa <forevermuyii@gmail.com>
2024-06-14 12:46:28 +0100

    integrated test doc

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores.faiss import FAISS
-from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.llms import OpenAI
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from llama_index.embeddings.langchain import LangchainEmbedding
+from langchain_google_genai.llms import GoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 
 logging.basicConfig(
@@ -16,9 +18,12 @@ logging.basicConfig(
 
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 DATABASE = None
 
+embed_model = HuggingFaceEmbeddings(model_name="thenlper/gte-large")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id,
@@ -32,14 +37,15 @@ async def load(update: Update, context: ContextTypes.DEFAULT_TYPE):
     docs = text_splitter.split_documents(documents)
 
     global DATABASE
-    DATABASE = FAISS.from_documents(docs, OpenAIEmbeddings())
+    DATABASE = FAISS.from_documents(docs, embedding=embed_model)
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text="Document loaded!")
 
 
 async def query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     docs = DATABASE.similarity_search(update.message.text, k=4)
-    chain = load_qa_chain(llm=OpenAI(), chain_type="stuff")
+    chain = load_qa_chain(llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY), 
+                          chain_type="stuff")
     results = chain({
         'input_documents': docs,
         "question": update.message.text
